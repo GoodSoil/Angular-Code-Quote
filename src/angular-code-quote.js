58b09a444a90e09c5d5930610821446264b6d644
dagilleland <dagilleland@shaw.ca>
2015-11-27 15:43:58 -0700

    <code-quote> directive
    Limitations:
    - PrismJS dependency
    - Presumes line-numbers

--- src/angular-code-quote.js
+++ src/angular-code-quote.js
@@ -30,4 +30,41 @@
       
       return factory;
      });
+     
+     clientApp.directive('codeQuote', function($timeout, CodeQuoteFactory) {
+        return {
+            restrict: 'E',
+            template: '<pre><code>{{ mycode.code }}</code></pre>',
+            replace: true,
+            scope: {
+                repo: '@'
+            },
+            controllerAs: 'mycode',
+            controller: function($scope, CodeQuoteFactory) {},
+            link: {
+                pre: function(scope, element, attrs) {
+                    CodeQuoteFactory.loadCode(scope.repo, codeQuoteSuccess, codeQuoteError);
+                    function codeQuoteSuccess(data, status, headers, config) {
+                        scope.mycode.code = data.code;
+                        scope.mycode.lang = data.lang;
+                        element.addClass('line-numbers');
+                        element.attr('data-start', data.request.start);
+                        // element.addClass('language-' + scope.mycode.lang);
+                        element.find('code').addClass('language-' + scope.mycode.lang);
+                        // Prism.highlightElement(element[0]);
+                        var e = element.find('code')[0];
+                        $timeout(function() {
+                            Prism.highlightElement(e);
+                            // Prism.highlightAll();
+                        }, 20);
+                        
+                    };
+                    
+                    function codeQuoteError(data, status, headers, config) {
+                        console.log(status);
+                    };
+                }
+            }
+        };
+    });
  })();
