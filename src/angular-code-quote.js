ce3c40739243de61972e6ec078a8d85edf07cb83
dagilleland <dagilleland@shaw.ca>
2015-11-27 14:00:21 -0700

    Initial module

---
+++ src/angular-code-quote.js
@@ -0,0 +1,11 @@
+/**
+ * @license Angular Code Quote v0.0.1
+ * (c) 2015. https://github.com/GoodSoil/Angular-Code-Quote
+ * License: MIT
+ */
+ 
+ (function () {
+     'use strict';
+     
+     var cq = angular.module('codequote', []);
+ })();
