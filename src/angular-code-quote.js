2bb725c641c9e8efee11c8eac7cee4a75e6594d9
dagilleland <dagilleland@shaw.ca>
2015-11-27 15:40:51 -0700

    Factory for API calls to CodeQuote

--- src/angular-code-quote.js
+++ src/angular-code-quote.js
@@ -8,4 +8,26 @@
      'use strict';
      
      var cq = angular.module('codequote', []);
+     cq.factory('apiFactory', function($http) {
+       var factory = {};
+       factory.loadCode = loadCode;
+       
+       // Hoised functions
+       /**
+       * Call the CodeQuote service to get the quote, with the supplied query string.
+       */
+      function loadCode(queryString, successCallback, errorCallback) {
+          if(angular.isString(queryString)) {
+              var quoteUrl = 'http://codequote.azurewebsites.net/quote' + queryString;
+              if(quoteUrl) {
+                  // Call code-quote
+                  $http.get(quoteUrl)
+                          .success(successCallback)
+                          .error(errorCallback);
+              }
+          }
+      };
+      
+      return factory;
+     });
  })();
